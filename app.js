d2682698ef8f908a4c5687c41822a472ee0c71dc
ChessRosati <60305508+ChessRosati@users.noreply.github.com>
2020-10-06 20:41:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,78 @@
-d3.csv("./cleaned_wine_v2(syrah).csv").then(function(data2) {
-    console.log(data2[0]);
-  });
-  
-  function _filter(list, predi) {
-      let newList = [];
-      for (let i = 0; i < list.length; i++) {
-          if (predi(list[i])) newList.push(list[i]);
-      }
-      return newList;
-  };
-  
-  let x_axis = [];
-  let y_axis = [];
-  let labels = [];
-  
-  for (var item in data2) {
-    //console.log(data2[item].points);
-    x_axis.push(data2[item].points);
-    y_axis.push(data2[item].price);
-    labels.push(data2[item].region_1);
-  }
-  
-  let trace = [];
-  
-  const uniqueLabels = [...new Set(labels)].sort();
-  //console.log(uniqueLabels);
-  for (let label of uniqueLabels) {
-      //create filtered array from data2
-      let filteredList= _filter(data2, (traceData) => traceData.region_1 == region_1);
-       
-      let xValueList = [];
-      let yValueList = [];
-      let textList = [];
-  
-      for(let item of filteredList){	 	
-         xValueList.push(item.points);
-         yValueList.push(item.price);
-         textList.push(item.region_1);
-      }
-  
-      trace.push({
-        x: xValueList,
-        y: yValueList,
-        mode: "markers",
-        type: "scatter",
-        text: textList,
-        showlegend: true,
-        name: region_1
-       });
-   
+Plotly.d3.csv("./cleaned_wine_v2(syrah).csv", function(err, rows){
+
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
     }
-  
-  Plotly.newPlot('plotly-div', trace);
-  
-  /*ONE_TRACE_SMAPLE*/
-  let data = {
-       x: x_axis, 
-      y: y_axis,
-        mode: "markers",
-        type: "scatter",
-        text: labels,
-      showlegend : true,
-      name : 'ONE_TRACE_SMAPLE'
-  }
-  Plotly.plot('plotly-div2', [data], {}, {showSendToCloud:true});
+
+    var allCountryNames = unpack(rows, 'country'),
+        allYear = unpack(rows, 'price'),
+        allGdp = unpack(rows, 'points'),
+        listofCountries = [],
+        currentCountry,
+        currentGdp = [],
+        currentYear = [];
+
+    for (var i = 0; i < allCountryNames.length; i++ ){
+        if (listofCountries.indexOf(allCountryNames[i]) === -1 ){
+            listofCountries.push(allCountryNames[i]);
+        }
+    }
+
+    function getCountryData(chosenCountry) {
+        currentGdp = [];
+        currentYear = [];
+        for (var i = 0 ; i < allCountryNames.length ; i++){
+            if ( allCountryNames[i] === chosenCountry ) {
+                currentGdp.push(allGdp[i]);
+                currentYear.push(allYear[i]);
+            }
+        }
+    };
+
+    // Default Country Data
+    setBubblePlot('USA');
+
+    function setBubblePlot(chosenCountry) {
+        getCountryData(chosenCountry);
+
+        var trace1 = {
+            x: currentYear,
+            y: currentGdp,
+            mode: 'markers',
+            marker: {
+                size: 12,
+                opacity: 0.5
+            }
+        };
+
+        var data = [trace1];
+
+        var layout = {
+            title:'Scatter Plot',
+            height: 400,
+            width: 480
+        };
+
+        Plotly.newPlot('myDiv', data, layout);
+    };
+
+    var innerContainer = document.querySelector('[data-num="0"'),
+        plotEl = innerContainer.querySelector('.plot'),
+        countrySelector = innerContainer.querySelector('.countrydata');
+
+    function assignOptions(textArray, selector) {
+        for (var i = 0; i < textArray.length;  i++) {
+            var currentOption = document.createElement('option');
+            currentOption.text = textArray[i];
+            selector.appendChild(currentOption);
+        }
+    }
+
+    assignOptions(listofCountries, countrySelector);
+
+    function updateCountry(){
+        setBubblePlot(countrySelector.value);
+    }
+
+    countrySelector.addEventListener('change', updateCountry, false);
+});
